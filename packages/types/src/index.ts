a2b2e2cc890e17b8984d1f857b7ccefcbbf41b4f
cagri <muysal.cagri@gmail.com>
2022-03-18 22:58:55 +0300

    feat(types): added something

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,6 +3,6 @@ export type MeaningOfLife = number;
 export function isMeaningOfLife(arg: any): arg is MeaningOfLife {
   if (arg === 42) return true;
 
-  // some change here
+  // some change here, some other change here
   return false;
 }
